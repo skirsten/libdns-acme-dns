cf496f50aa61866aecd29dd2df75f9812fd9b142
Simon Kirsten <simonkirsten24@gmail.com>
2021-10-09 16:51:57 +0200

    Initial push

--- README.md
+++ README.md
@@ -1,26 +1,8 @@
-**DEVELOPER INSTRUCTIONS:**
+# [`acme-dns`](https://github.com/joohoi/acme-dns) for [`libdns`](https://github.com/libdns/libdns)
 
-This repo is a template for developers to use when creating new [libdns](https://github.com/libdns/libdns) provider implementations.
+[![Go Reference](https://pkg.go.dev/badge/test.svg)](https://pkg.go.dev/github.com/skirsten/libdns-acme-dns)
 
-Be sure to update:
+This package implements the [libdns interfaces](https://github.com/libdns/libdns) for [acme-dns](https://github.com/joohoi/acme-dns), allowing you to manage DNS records.
 
-- The package name
-- The Go module name in go.mod
-- The latest `libdns/libdns` version in go.mod
-- All comments and documentation, including README below and godocs
-- License (must be compatible with Apache/MIT)
-- All "TODO:"s is in the code
-- All methods that currently do nothing
-
-Remove this section from the readme before publishing.
-
----
-
-\<PROVIDER NAME\> for [`libdns`](https://github.com/libdns/libdns)
-=======================
-
-[![Go Reference](https://pkg.go.dev/badge/test.svg)](https://pkg.go.dev/github.com/libdns/TODO:PROVIDER_NAME)
-
-This package implements the [libdns interfaces](https://github.com/libdns/libdns) for \<PROVIDER\>, allowing you to manage DNS records.
-
-TODO: Show how to configure and use. Explain any caveats.
+- As acme-dns is not a fully featured DNS server, this package has many limitations. This makes it only usable for performing ACME challenges.
+- Currently does not take into consideration the zone. To manage multiple zones, multiple instances of the provider must be created.
--- provider.go
+++ provider.go
@@ -1,47 +1,111 @@
-// Package libdnstemplate implements a DNS record management client compatible
-// with the libdns interfaces for <PROVIDER NAME>. TODO: This package is a
-// template only. Customize all godocs for actual implementation.
-package libdnstemplate
+// Package libdnsacmedns implements a DNS record management client compatible
+// with the libdns interfaces for acme-dns.
+package libdnsacmedns
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"path"
+	"regexp"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/libdns/libdns"
 )
 
-// TODO: Providers must not require additional provisioning steps by the callers; it
-// should work simply by populating a struct and calling methods on it. If your DNS
-// service requires long-lived state or some extra provisioning step, do it implicitly
-// when methods are called; sync.Once can help with this, and/or you can use a
-// sync.(RW)Mutex in your Provider struct to synchronize implicit provisioning.
-
-// Provider facilitates DNS record manipulation with <TODO: PROVIDER NAME>.
+// Provider facilitates DNS record manipulation with acme-dns.
 type Provider struct {
-	// TODO: put config fields here (with snake_case json
-	// struct tags on exported fields), for example:
-	APIToken string `json:"api_token,omitempty"`
+	Endpoint  string `json:"endpoint,omitempty"`
+	Username  string `json:"username,omitempty"`
+	Password  string `json:"password,omitempty"`
+	Subdomain string `json:"subdomain,omitempty"`
+}
+
+type UpdateBody struct {
+	Subdomain string `json:"subdomain"`
+	Value     string `json:"txt"`
 }
 
 // GetRecords lists all the records in the zone.
 func (p *Provider) GetRecords(ctx context.Context, zone string) ([]libdns.Record, error) {
-	return nil, fmt.Errorf("TODO: not implemented")
+	return nil, fmt.Errorf("not implemented")
+}
+
+// From https://github.com/joohoi/acme-dns/blob/68bb6ab654b6fb1fe375e08807688c55621513a2/util.go#L60
+func sanitizeString(s string) string {
+	// URL safe base64 alphabet without padding as defined in ACME
+	re, _ := regexp.Compile(`[^A-Za-z\-\_0-9]+`)
+	return re.ReplaceAllString(s, "")
+}
+
+// From https://github.com/joohoi/acme-dns/blob/68bb6ab654b6fb1fe375e08807688c55621513a2/validation.go#L34
+func validTXT(s string) bool {
+	sn := sanitizeString(s)
+	if utf8.RuneCountInString(s) == 43 && utf8.RuneCountInString(sn) == 43 {
+		// 43 chars is the current LE auth key size, but not limited / defined by ACME
+		return true
+	}
+	return false
+}
+
+func (p *Provider) updateRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
+	if len(records) != 1 {
+		return nil, fmt.Errorf("not implemented")
+	}
+
+	rec := records[0]
+
+	if rec.Type != "TXT" || !strings.HasPrefix(rec.Name, "_acme_challenge") || !validTXT(rec.Value) {
+		return nil, fmt.Errorf("not implemented")
+	}
+
+	body, _ := json.Marshal(UpdateBody{
+		Subdomain: p.Subdomain,
+		Value:     rec.Value,
+	})
+
+	request, err := http.NewRequestWithContext(ctx, "POST", path.Join(p.Endpoint, "update"), bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("content-type", "application/json")
+	request.Header.Set("x-api-user", p.Username)
+	request.Header.Set("x-api-key", p.Password)
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(
+			"acme-dns update failed: POST %s, subdomain: %s, value: %s  %s",
+			response.Request.RequestURI, p.Subdomain, rec.Value, response.Status,
+		)
+	}
+
+	return records, nil
 }
 
 // AppendRecords adds records to the zone. It returns the records that were added.
 func (p *Provider) AppendRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
-	return nil, fmt.Errorf("TODO: not implemented")
+	return p.updateRecords(ctx, zone, records)
 }
 
 // SetRecords sets the records in the zone, either by updating existing records or creating new ones.
 // It returns the updated records.
 func (p *Provider) SetRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
-	return nil, fmt.Errorf("TODO: not implemented")
+	return p.updateRecords(ctx, zone, records)
 }
 
 // DeleteRecords deletes the records from the zone. It returns the records that were deleted.
 func (p *Provider) DeleteRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
-	return nil, fmt.Errorf("TODO: not implemented")
+	// delete flow is not implemented by acme-dns yet: https://github.com/joohoi/acme-dns/search?q=delete&type=issues
+	return nil, nil
 }
 
 // Interface guards
